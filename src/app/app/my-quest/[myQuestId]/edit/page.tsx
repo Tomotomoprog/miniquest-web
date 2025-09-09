4c67ac40a5e964588e8c515377a8f10362c8d1c9
Tomotomoprog <tomoyasumuk@gmail.com>
2025-09-09 17:39:10 +0900

    エラー改善

--- src/app/app/my-quest/[myQuestId]/edit/page.tsx
+++ src/app/app/my-quest/[myQuestId]/edit/page.tsx
@@ -1,9 +1,13 @@
 "use client";
 import { useFetchMyQuestById } from "@/hooks/useMyQuests";
 import MyQuestCreationForm from "../../MyQuestCreationForm";
+import { useParams } from "next/navigation"; // 👈 useParamsをインポート
 
-export default function EditMyQuestPage({ params }: { params: { myQuestId: string } }) {
-  const { myQuestId } = params;
+// ページコンポーネントの引数からparamsを削除
+export default function EditMyQuestPage() {
+  const params = useParams(); // 👈 useParamsフックを使ってパラメータを取得
+  const myQuestId = params.myQuestId as string; // 👈 取得したパラメータからIDを取り出す
+
   const { data: quest, isLoading } = useFetchMyQuestById(myQuestId);
 
   if (isLoading) {
